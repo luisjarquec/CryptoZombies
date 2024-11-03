ce875c4626da2c1564478e096397766d1fd6cc0f
luisjarquec <luis.jarquec@gmail.com>
2024-11-04 00:00:37 +0100

    Chapter 2: Withdraws

--- zombies/zombiefeeding.sol
+++ zombies/zombiefeeding.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 
-import "./zombies/zombiefactory.sol";
+import "./zombiefactory.sol";
 
 abstract contract KittyInterface {
   function getKitty(uint256 _id) external virtual view returns (
--- zombies/zombiehelper.sol
+++ zombies/zombiehelper.sol
@@ -5,9 +5,16 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+    uint levelUpFee = 0.001 ether;
+
     modifier aboveLevel(uint _level, uint _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
+    }
+
+    function levelUp(uine _zombieId) external payable {
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
     }
 
     function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId){
@@ -21,6 +28,15 @@ contract ZombieHelper is ZombieFeeding {
     }
 
     function getZombiesByOwner(address _owner) external view returns (uint[] memory){
+        uint[] memory result = new uint[](ownerZombieCount[_owner]);
 
+        uint counter = 0;
+        for(uint i=0; i < zombies.length; i++){
+            if(zombieToOwner[i] == _owner){
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
     }
 }
