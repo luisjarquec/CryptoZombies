3a8ad3f77ae216fc61533d99bb0fb8d51becf7e7
luisjarquec <luis.jarquec@gmail.com>
2024-11-03 19:08:52 +0100

    Chapter 11: Storage is Expensive

--- zombies/zombiefactory.sol
+++ zombies/zombiefactory.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./ownable.sol";
 
-contract ZombieFactory {
+contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
-    
+    uint32 cooldownTime = 1 days;
+
     struct Zombie {
         string name;
         uint dna;
+        uint32 level;
+        uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -20,7 +24,7 @@ contract ZombieFactory {
     mapping (address => uint) public ownerZombieCount;
 
     function _createZombie (string memory _name, uint _dna) internal {
-        zombies.push(Zombie(_name, _dna));
+        zombies.push(Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime)));
         uint id = zombies.length - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
---
+++ zombies/zombiehelper.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+
+    modifier aboveLevel(uint _level, uint _zombieId) {
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
+
+    function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId){
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+
+    function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId){
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    }
+
+    function getZombiesByOwner(address _owner) external view returns (uint[] memory){
+
+    }
+}
